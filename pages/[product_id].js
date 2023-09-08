fa2ce7961e1512896d7f7cf6b5cb8109f1b0db41
JadFu <yfu5@aceproject.space>
2023-09-08 12:13:18 -0500

    build project page

---
+++ pages/[product_id].js
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import fs from "fs/promises";
+import path from "path";
+
+export default function productDetail(props) {
+  const { loadedProduct } = props;
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  //this 'params' is not a custom name
+  const { params } = context;
+  const productId = params.product_id;
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import path from "path";
 //file system module from node.js
 import fs from "fs/promises";
+import path from "path";
+import Link from "next/link";
 
 export default function Home(props) {
   const { products } = props;
@@ -8,13 +9,14 @@ export default function Home(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}><Link>{product.title}</Link></li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  console.log('RE - Generating ...');
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -23,8 +25,17 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
-    //page will re-generate every 10 sec or slower
+    //page will re-generate every 60 sec or slower
     //this only works on build, will not work on dev
-    revalidate: 10
+    revalidate: 60,
+
+    //this will force user visit 404 when visit this page
+    //usually used when data fetch failed to redirect to 404
+    //notFound: true
+
+    //this will redirect user to custom routes
+    //redirect: {
+    //  destination: '/no-data'
+    //}
   };
 }
