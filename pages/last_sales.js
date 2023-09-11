39a941233962f9c61acb8e44662f55c2347401be
JadFu <yfu5@aceproject.space>
2023-09-11 11:34:18 -0500

    combine custom data fetching with server side pre-rendering

--- pages/last_sales.js
+++ pages/last_sales.js
@@ -1,38 +1,71 @@
 import { useEffect, useState } from "react";
+//this requires 'npm install swr'
+import useSWR from "swr";
 
-export default function LastSale() {
-  const [sales, setSales] = useState();
-  const [loading, setLoading] = useState(false);
+export default function LastSale(props) {
+  const [sales, setSales] = useState(props.sales);
+  //   const [loading, setLoading] = useState(false);
+
+  //only useSWR will not work, add defalut fetch like follow:
+  //useSWR(<request-url>, (url) => fetch(url).then(res => res.json()))
+  const { data, error } = useSWR(
+    "https://next-course-fetching-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setLoading(true);
-    fetch("https://next-course-fetching-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-        setSales(transformedSales);
-        setLoading(false);
-      });
-  }, []);
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (loading) {
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     fetch("https://next-course-fetching-default-rtdb.firebaseio.com/sales.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+
+  //         setSales(transformedSales);
+  //         setLoading(false);
+  //       });
+  //   }, []);
+
+  //   if (loading) {
+  //     return <p>Loading ...</p>;
+  //   }
+
+  //error should be check before data
+  if (error) {
+    return <p>Failed to Load Required Data</p>;
+  }
+  if (!data && !sales) {
     return <p>Loading ...</p>;
   }
 
   //this is for the first time rendering
   //where sales state is still undefind
-  if (!sales) {
-    return <p>No data is found ...</p>
-  }
+  //   if (!sales) {
+  //     return <p>No data is found ...</p>;
+  //   }
 
   return (
     <ul>
@@ -43,4 +76,22 @@ export default function LastSale() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-course-fetching-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales }, revalidate: 10 };
 }
